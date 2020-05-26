4d3a63de60e2b7a2231afc12b5532cc7ea8fe63e
ArjoNagelhout <arjo.nagelhout@gmail.com>
2020-05-26 15:31:50 +0200

    Nu met het filteren van data en het kunnen instellen van een minimum en maximum afstand

--- goed_werkende_code/goed_werkende_code.ino
+++ goed_werkende_code/goed_werkende_code.ino
@@ -1,45 +1,47 @@
 
 #define PIN_NUM 3
 int  data[PIN_NUM]; //data array
-char dataID[PIN_NUM] = {'A','B','C'}; //data label
-int trigPin[PIN_NUM]={2,9,6};
-int echoPin[PIN_NUM]={3,10,7};
-int sampleRate = 500;
+char dataID[PIN_NUM] = {'A', 'B', 'C'}; //data label
+int trigPin[PIN_NUM] = {14, 16, 18};
+int echoPin[PIN_NUM] = {13, 15, 17};
+int sampleRate = 2000;
+
+int minDistance = 2; // in cm
+int maxDistance = 100; // in cm
 
 long timer = micros();
 void setup() {
   Serial.begin (115200);
- for (int i = 0 ; i < PIN_NUM ; i++) {
- 
- pinMode(trigPin[i], OUTPUT);
-  pinMode(echoPin[i], INPUT);
+  for (int i = 0 ; i < PIN_NUM ; i++) {
 
-}
+    pinMode(trigPin[i], OUTPUT);
+    pinMode(echoPin[i], INPUT);
+
+  }
 }
 
 void loop() {
- 
-  if (micros() - timer >= 1000000/sampleRate) { //Timer: send sensor data in every 10ms
+
+  if (micros() - timer >= 1000000 / sampleRate) {
     timer = micros();
-      for (int i = 0 ; i < PIN_NUM ; i++) {
-        long duration[i], distance[i];
-  digitalWrite(trigPin[i], LOW);  
-  delayMicroseconds(2); 
-  digitalWrite(trigPin[i], HIGH);
-  delayMicroseconds(10); 
-  digitalWrite(trigPin[i], LOW);
-  duration[i] = pulseIn(echoPin[i], HIGH);
-  distance[i] = (duration[i]/2) / 29.1;
-   if (distance[i] >= 500 || distance[i] <= 0){
-    Serial.println("Out of range");
-   }
-    else{
-  data[i]= distance[i];
-delay(10);
-      sendDataToProcessing(dataID[i], data[i]);
-}
-}
-}
+    for (int i = 0 ; i < PIN_NUM ; i++) {
+      long duration, distance;
+      digitalWrite(trigPin[i], LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin[i], HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin[i], LOW);
+      duration = pulseIn(echoPin[i], HIGH);
+      distance = (duration / 2) / 29.1; // Gets the distance in cm
+      distance = map(distance, minDistance, maxDistance, 0, 1024);
+      
+
+      if ((distance >= 0) && (distance <= 1024)) {
+        data[i] = distance;
+        sendDataToProcessing(dataID[i], data[i]);
+      }
+    }
+  }
 }
 
 void sendDataToProcessing(char symbol, int data) {
