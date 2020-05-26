394e6ef9d2b9fe6f2103718054aa4fc971b1b689
jeaninedeleeuw <j-de-leeuw@hotmail.com>
2020-05-26 14:34:43 +0200

    code arduino
    code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # IIP2020
 Interactive Intelligent Products
 Aangepast 
+
+Werkend gekregen met 3 sensoren ook getest met het programma p6_2d_A012 er lijkt alleen wel een vertraging in te zitten
---
+++ goed_werkende_code/goed_werkende_code/goed_werkende_code.ino
@@ -0,0 +1,44 @@
+
+#define PIN_NUM 3
+int  data[PIN_NUM]; //data array
+char dataID[PIN_NUM] = {'A','B','C'}; //data label
+int trigPin[PIN_NUM]={2,9,6};
+int echoPin[PIN_NUM]={3,10,7};
+int sampleRate = 500;
+
+long timer = micros();
+void setup() {
+  Serial.begin (115200);
+ for (int i = 0 ; i < PIN_NUM ; i++) {
+ 
+ pinMode(trigPin[i], OUTPUT);
+  pinMode(echoPin[i], INPUT);
+
+}
+}
+
+void loop() {
+ 
+  if (micros() - timer >= 1000000/sampleRate) { //Timer: send sensor data in every 10ms
+    timer = micros();
+      for (int i = 0 ; i < PIN_NUM ; i++) {
+        long duration[i], distance[i];
+  digitalWrite(trigPin[i], LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin[i], HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin[i], LOW);
+  duration[i] = pulseIn(echoPin[i], HIGH);
+  distance[i] = (duration[i]/2) / 29.1;
+//  dataID 
+  data[i]= distance[i];
+delay(100);
+      sendDataToProcessing(dataID[i], data[i]);
+}
+}
+}
+
+void sendDataToProcessing(char symbol, int data) {
+  Serial.print(symbol);  // symbol prefix of data type
+  Serial.println(data);  // the integer data with a carriage return
+}
